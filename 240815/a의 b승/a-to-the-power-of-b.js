82a32b35be6356166bb02d47b41394a7784e08df
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:25:31 +0900

    Title: a의 b승, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 240815/a의 b승/a-to-the-power-of-b.js
+++ 240815/a의 b승/a-to-the-power-of-b.js
@@ -3,6 +3,8 @@ const fs = require("fs")
 let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 let result= 1
 
-result = a*b
+for (let i = 0; i < b; i++) {
+    result *= a;
+}
 
 console.log(result)
