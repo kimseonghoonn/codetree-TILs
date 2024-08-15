5835d1b343a5e8a6121ee8a6060705f2dabd5495
kimseonghoonn <kdr05103@naver.com>
2024-08-15 14:25:22 +0900

    Title: 합성수 판별기, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240815/합성수 판별기/factorization-discriminator.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+for(let i=2; i<n; i++) {
+    if (n%i!=0) {
+        continue
+    }
+    else {
+        console.log("C")
+        break
+    }
+}
