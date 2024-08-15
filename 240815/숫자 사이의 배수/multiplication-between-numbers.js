489b86c5f2fc7207bbbb857f8d6add051f8d1815
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:00:23 +0900

    Title: 숫자 사이의 배수, Time: 203ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/숫자 사이의 배수/multiplication-between-numbers.js
+++ 240815/숫자 사이의 배수/multiplication-between-numbers.js
@@ -4,7 +4,7 @@ let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 let result= 0
 
 for (let i=a; i<=b; i++) {
-    if(x%5==0 && x%7==0) {
+    if(i%5==0 && i%7==0) {
         result +=i
     }
 }
