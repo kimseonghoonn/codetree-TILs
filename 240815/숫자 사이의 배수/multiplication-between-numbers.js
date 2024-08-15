6c56887956c61fd2eeb031dc4f54a8be984691c0
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:02:54 +0900

    Title: 숫자 사이의 배수, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 240815/숫자 사이의 배수/multiplication-between-numbers.js
+++ 240815/숫자 사이의 배수/multiplication-between-numbers.js
@@ -2,11 +2,13 @@ const fs = require("fs")
 
 let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 let result= 0
+let count = 0
 
 for (let i=a; i<=b; i++) {
-    if(i%5==0 && i%7==0) {
+    if(i%5==0 || i%7==0) {
         result +=i
+        count++
     }
 }
 
-console.log(result)
+console.log(result + " " + (result/count).toFixed(1))
