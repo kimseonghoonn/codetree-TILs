ac1301c435ad43362f12d918790726d0cde33932
kimseonghoonn <kdr05103@naver.com>
2024-08-15 14:29:34 +0900

    Title: 합성수 판별기, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/합성수 판별기/factorization-discriminator.js
+++ 240815/합성수 판별기/factorization-discriminator.js
@@ -5,9 +5,11 @@ for(let i=2; i<n; i++) {
     if (n%i!=0) {
         continue
     }
+    else if(i==(n-1)) {
+        console.log("N")
+    }
     else {
         console.log("C")
         break
     }
-    console.log("N") 
 }
