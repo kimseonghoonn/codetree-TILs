9d1673cfc1eb8c905c87a3b740a86c85704dbd17
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:30:29 +0900

    Title: a의 배수들의 곱, Time: 172ms, Memory: 14MB, Status: Passed - CodeTree

--- 240815/a의 배수들의 곱/product-of-multiples-of-a.js
+++ 240815/a의 배수들의 곱/product-of-multiples-of-a.js
@@ -3,8 +3,8 @@ const fs = require("fs")
 let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 let result= 1
 
-for (let i = 1; i < b; i++) {
-    if(b%a==0) {
+for (let i = 1; i <= b; i++) {
+    if(i%a==0) {
         result *= i;
     }
 }
