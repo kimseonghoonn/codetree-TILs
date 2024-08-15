e44d452d709a8d3e49e1f6d485ae25d333ede347
kimseonghoonn <kdr05103@naver.com>
2024-08-15 10:58:19 +0900

    Title: 홀수만 더하기, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/홀수만 더하기/add-only-odd-numbers.js
+++ 240815/홀수만 더하기/add-only-odd-numbers.js
@@ -1,12 +1,12 @@
 const fs = require("fs")
 
 let input = fs.readFileSync(0).toString().trim().split("\n").map(Number)
-let n = Number(input[0])
+let n = Number(input[0])      //n줄 첫번째 줄에 나옴.
 let result= 0
 
-for (let i=0; i<n; i++) {
-    let x = Number(input[i])
-    if(x%2==0 && x%3==0) {
+for (let i=1; i<=n; i++) {     //그래서 이 개수까지 반복
+    let x = Number(input[i])  //x는  
+    if(x%2==1 && x%3==0) {
         result +=n[i]
     }
 }
