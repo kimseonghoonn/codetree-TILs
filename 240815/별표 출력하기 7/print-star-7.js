c71e919e64b4a68877ee848ac7fdacc602919583
kimseonghoonn <kdr05103@naver.com>
2024-08-15 16:12:52 +0900

    Title: 별표 출력하기 7, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240815/별표 출력하기 7/print-star-7.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+
+let n = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let str = ""
+
+for (let i=0; i<n; i++) {
+    str = ""
+    for (let j=0; j<=i; j++) {
+        str +="* ";
+    }
+    console.log(str)
+}
