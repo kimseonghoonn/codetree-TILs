5bcf5c906daffbb9212f66dc31fd916179c66dec
kimseonghoonn <kdr05103@naver.com>
2024-08-15 18:59:44 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 172ms, Memory: 14MB, Status: Passed - CodeTree

--- 240815/시험 통과 여부 확인하기 2/verify-test-passed-2.js
+++ 240815/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 let n = fs.readFileSync(0).toString().trim().split("\n")
 let gr= Number(n[0])
-let test = n[1].split(" ").map(Number)
+let summ=0
 
-
-for (let i=0; i<n; i++) {
+for (let i=1; i<=gr; i++) {
+    let test = n[i].split(" ").map(Number)
     let sum=0
+
     for (let j=0; j<4; j++) {
-        sum += test[i]
+        sum += test[j]
     }
-    let avg=0
-    avg = sum/4
 
-    if(avg>=60) {
+    let avg = sum / 4
+
+    if(avg >= 60) {
         console.log("pass")
+        summ++
     }
 
-    else if(avg<60) {
+    else {
         console.log("fail")
     }
 }
+
+console.log(summ)
