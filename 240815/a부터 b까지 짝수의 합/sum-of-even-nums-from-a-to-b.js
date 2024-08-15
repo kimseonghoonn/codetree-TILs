0f431c4f4ea47285961c0eee04c110520665b51b
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:05:31 +0900

    Title: a부터 b까지 짝수의 합, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240815/a부터 b까지 짝수의 합/sum-of-even-nums-from-a-to-b.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+
+let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+let result= 0
+
+for (let i=a; i<=b; i++) {
+    if(i%2==0) {
+        result +=i
+    }
+}
+
+console.log(result)
