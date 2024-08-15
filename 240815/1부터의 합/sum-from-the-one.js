ceb6aef0a99dfa999f6306aa1097ad57804a4049
kimseonghoonn <kdr05103@naver.com>
2024-08-15 12:27:21 +0900

    Title: 1부터의 합, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240815/1부터의 합/sum-from-the-one.js
+++ 240815/1부터의 합/sum-from-the-one.js
@@ -5,13 +5,9 @@ let n = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 let result = 0
 
 for(let i=1; i<=100; i++) {
-    if (result<=n) {
-        result +=i
-    }
-    else {
-        result -=(i-1)
+    result +=i
+    if (result>=n) {
+        console.log(i)
         break;
     }
 }
-
-console.log(result)
