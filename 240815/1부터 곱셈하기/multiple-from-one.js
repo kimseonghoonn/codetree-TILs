f46dd24d14b407fdc567dae9827584b3eefc5487
kimseonghoonn <kdr05103@naver.com>
2024-08-15 12:27:36 +0900

    Title: 1부터 곱셈하기, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240815/1부터 곱셈하기/multiple-from-one.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let n = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let result = 0
+
+for(let i=1; i<=100; i++) {
+    result *=i
+    if (result>=n) {
+        console.log(i)
+        break;
+    }
+}
