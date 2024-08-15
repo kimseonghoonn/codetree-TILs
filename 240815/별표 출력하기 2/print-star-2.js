794a23228ad8dc8878d9606d395ab726fb4e8d39
kimseonghoonn <kdr05103@naver.com>
2024-08-15 15:16:36 +0900

    Title: 별표 출력하기 2, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/별표 출력하기 2/print-star-2.js
+++ 240815/별표 출력하기 2/print-star-2.js
@@ -4,9 +4,9 @@ let n = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 
 let str = ""
 
-for (let i=0; i<n; i++) {
+for (let i=0; i<n[i]; i++) {
     str = ""
-    for (let j=0; j<n-i; j++) {
+    for (let j=0; j<n[i]-i; j++) {
         str +="*"
     }
     console.log(str)
