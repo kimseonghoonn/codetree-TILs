fe8c421ab663832efc9dbb9cbc184b798968cc29
kimseonghoonn <kdr05103@naver.com>
2024-08-15 15:07:27 +0900

    Title: 정사각형 두 개 출력, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240815/정사각형 두 개 출력/output-two-rectangles.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+let n = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let str = "";
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
+console.log("");
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
