71577a5b93e9ee03dc35087ca4642fdbb93cda17
kimseonghoonn <kdr05103@naver.com>
2024-08-15 12:33:58 +0900

    Title: 1까지 나누기, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/1까지 나누기/divide-by-1.js
+++ 240815/1까지 나누기/divide-by-1.js
@@ -6,10 +6,12 @@ let result = n
 let count=0
 
 for(let i=1; i<=100; i++) {
-    result /= i
-    count++
+    if(result>1) {
+        result /= i
+        count++
+    }
 
-    if (result<=1) {
+    else if (result<=1) {
         break;
     }
 }
