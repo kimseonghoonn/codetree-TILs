50d03fb0961995b57d6bf25c0a00a83938a6ae08
kimseonghoonn <kdr05103@naver.com>
2024-08-15 17:51:28 +0900

    Title: 특정 숫자 도달하기, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240815/특정 숫자 도달하기/reaching-specific-number.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let n = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let sum = 0;
+let count = 0;
+
+for (let elem of n) {
+    if(elem >= 250) {
+        break;
+    }
+    sum +=elem
+    count++
+}
+
+console.log(sum, (sum/count))
