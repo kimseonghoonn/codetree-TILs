3d9315719b33232773712262f1eb4e5cfe3e8a13
kimseonghoonn <kdr05103@naver.com>
2024-08-15 10:59:54 +0900

    Title: 숫자 사이의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240815/숫자 사이의 배수/multiplication-between-numbers.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+
+let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+let result= 0
+
+for (let i=a; i<=b; i++) {
+    if(x%5==0 && x%7==0) {
+        result +=x
+    }
+}
+
+console.log(result)
