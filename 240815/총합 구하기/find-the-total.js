2c1701d3ac2e9997bbff6a425d618cfea1060f10
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:21:40 +0900

    Title: 총합 구하기, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240815/총합 구하기/find-the-total.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+let [a,b] = fs.readFileSync(0).toString().trim().split("\n").map(Number)
+let result= 0
+
+for (let i=a; i<=b; i++) {
+    result +=i
+}
+
+console.log(result)
