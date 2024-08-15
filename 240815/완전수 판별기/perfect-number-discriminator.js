1109f335ddb6c5bfbd8fd0f5da6b12a9c26f1faf
kimseonghoonn <kdr05103@naver.com>
2024-08-15 11:13:40 +0900

    Title: 완전수 판별기, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240815/완전수 판별기/perfect-number-discriminator.js
@@ -0,0 +1,18 @@
+const fs = require("fs")
+
+let n = fs.readFileSync(0).toString().trim().split("\n").map(Number)
+let result= 0
+let count = 0
+
+for (let i=1; i<=n; i++) {
+    if(n%i==0) {
+        count++
+    }
+}
+
+if(count>=1) {
+        console.log("P")
+    }
+    else {
+        console.log("N")
+    }
