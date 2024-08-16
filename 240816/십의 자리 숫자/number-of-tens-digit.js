31dbd8e41e3b5b108f6ec1ffe413e308b2c1a358
kimseonghoonn <kdr05103@naver.com>
2024-08-16 17:28:41 +0900

    Title: 십의 자리 숫자, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 240816/십의 자리 숫자/number-of-tens-digit.js
+++ 240816/십의 자리 숫자/number-of-tens-digit.js
@@ -3,10 +3,11 @@ const fs = require("fs")
 let a = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 
 let countArr = Array(10).fill(0)
-
-for (let i=0; i<10; i++) {
-    if(a[i]==0) break;
-    countArr[parseInt(a[i]/10)]++
+let count = 0;
+while(true) {
+    if(a[count]==0) break;
+    countArr[parseInt(a[count]/10)]++
+    count++
 }
 
 for (let i=1; i<=9; i++) {
