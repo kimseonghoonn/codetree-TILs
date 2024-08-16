52c9cac1770cbc0b1330bd29a07f026155580246
kimseonghoonn <kdr05103@naver.com>
2024-08-16 19:30:14 +0900

    Title: n개의 숫자 중 최소, Time: 190ms, Memory: 20MB, Status: Passed - CodeTree

--- 240816/n개의 숫자 중 최소/min-of-n-num.js
+++ 240816/n개의 숫자 중 최소/min-of-n-num.js
@@ -3,10 +3,12 @@ let input = fs.readFileSync(0).toString().trim().split("\n")
 let n = Number(input[0])
 let x = input[1].split(" ").map(Number)
 let count = 0
-let max = Number.MAX_SAFE_INTEGER;
-let min = 0
-for (let i=0; i<n; i++) {
-    if (max>=x[i]) min = x[i]
+let min = x[0]
+
+for (let i=1; i<n; i++) {
+    if (min > x[i]) {
+        min = x[i]
+    }
 }
 
 for (let i=0; i<n; i++) {
