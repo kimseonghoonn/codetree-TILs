c004dca05f80be837f32009d14f58864ad57f4d2
kimseonghoonn <kdr05103@naver.com>
2024-08-17 20:30:17 +0900

    Title: 문자열 길이의 합, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240817/문자열 길이의 합/sum-length-of-string.js
+++ 240817/문자열 길이의 합/sum-length-of-string.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let cnt=0
 let count = 0
-for (let i=1; i<input[0]; i++) {
+for (let i=1; i<=input[0]; i++) {
     cnt +=input[i].length
     for (let j=0; j<input[i].length; j++) {
         if (input[i][j] == 'a') {
