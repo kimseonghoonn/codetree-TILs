919744ad72160704d6929cb85401786de9fa01e8
kimseonghoonn <kdr05103@naver.com>
2024-08-17 17:43:39 +0900

    Title: 더 긴 문자열, Time: 241ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240817/더 긴 문자열/longer-string.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+
+let [str1 , str2] = fs.readFileSync(0).toString().trim().split(" ")
+
+let len1 = str1.length
+let len2 = str2.length
+
+if (len1 > len2) {
+    console.log("Coding" + " " + len1)
+}
+else if (len1 < len2) {
+    console.log("Coding" + " " + len2)
+}
+else {
+    console.log("same")
+}
