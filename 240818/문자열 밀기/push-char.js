7b43a7d344f186c8f9d1f5578068d5ca50f485d5
kimseonghoonn <kdr05103@naver.com>
2024-08-18 17:07:23 +0900

    Title: 문자열 밀기, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240818/문자열 밀기/push-char.js
+++ 240818/문자열 밀기/push-char.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 let a = fs.readFileSync(0).toString().trim()
 
 if (a[0] != a[-1]) {
-    a = a.slice(-(a.length-1)) + a.slice(0,1)
+a = a.slice(-(a.length-1)) + a.slice(0,1)
 }
+
 
 console.log(a)
