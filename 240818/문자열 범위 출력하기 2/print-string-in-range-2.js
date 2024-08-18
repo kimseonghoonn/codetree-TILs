fcad36490f0ff46bb846a3429c1b38e2f33a3fd2
kimseonghoonn <kdr05103@naver.com>
2024-08-18 14:47:09 +0900

    Title: 문자열 범위 출력하기 2, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240818/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let givenStr = fs.readFileSync(0).toString().trim().split("\n");
+let a = givenStr[0].split("")
+let b = Number(givenStr[1])
+let str = ""
+
+if (a.length>b) {
+    for (let i = (a.length-1); i>=((a.length)-b); i--) {
+        str += a[i]
+    }
+}
+else if (a.length<b) {
+    for (let i= 0; i<b; i++) {
+        str += a[i]
+    }
+}
+
+console.log(str);
