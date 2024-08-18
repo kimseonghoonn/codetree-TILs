566493a67263a44b462094dbf6dba7e8588cb7c9
kimseonghoonn <kdr05103@naver.com>
2024-08-18 16:03:23 +0900

    Title: 문자열 전부 붙이기, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240818/문자열 전부 붙이기/paste-all-string.js
+++ 240818/문자열 전부 붙이기/paste-all-string.js
@@ -1,11 +1,11 @@
 const fs = require("fs")
 
-let input = fs.readFileSync(0).toString().trim()
+let input = fs.readFileSync(0).toString().trim().split("\n")
 let n = Number(input[0])
 let str=""
 
-for (let i=1; i<n; i++) {
+for (let i=1; i<=n; i++) {
     str += input[i]
 }
 
-conosole.log(str)
+console.log(str)
