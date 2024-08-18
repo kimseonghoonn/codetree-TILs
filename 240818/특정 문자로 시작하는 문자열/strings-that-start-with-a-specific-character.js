38256baf14d5a8dfd03b1094961b86b2a73bd5ba
kimseonghoonn <kdr05103@naver.com>
2024-08-18 14:31:46 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240818/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
+++ 240818/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split("\n")
 let count = 0
 let avg = 0
 
-for (let i=1; i<input[0]; i++) {
+for (let i=1; i<input[0]-1; i++) {
     if(input[i][0] == input[(input.length-1)]) {
         count++
         avg += input[i].length
