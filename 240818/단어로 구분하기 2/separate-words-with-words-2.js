41f14b9c7564acf86f313548f674686e75615694
kimseonghoonn <kdr05103@naver.com>
2024-08-18 14:03:23 +0900

    Title: 단어로 구분하기 2, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

--- 240818/단어로 구분하기 2/separate-words-with-words-2.js
+++ 240818/단어로 구분하기 2/separate-words-with-words-2.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(" ").join
+let input = fs.readFileSync(0).toString().trim().split(" ")
 
 for (let i=0; i<input.length; i++) {
     if (i%2==0) {
