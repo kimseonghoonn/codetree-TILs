cdbe5a3c4260fba1afdc993fed90d59cdf57c07e
kimseonghoonn <kdr05103@naver.com>
2024-08-18 14:18:14 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 232ms, Memory: 14MB, Status: Passed - CodeTree

--- 240818/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240818/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n")
-
+let o = "false"
 for (let i=0; i<10; i++) {
     if(input[i][(input[i].length-1)] == input[10]) {
-        console.log(input[i]) 
+        console.log(input[i])
+        o = "true" 
     }
+}
+
+if (o=="false") {
+    console.log("None")
 }
