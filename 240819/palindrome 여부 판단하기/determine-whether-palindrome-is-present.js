92846fe2ecc131de7d65b46f1cacaf1e6bc49365
kimseonghoonn <kdr05103@naver.com>
2024-08-19 15:27:50 +0900

    Title: palindrome 여부 판단하기, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240819/palindrome 여부 판단하기/determine-whether-palindrome-is-present.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let a= fs.readFileSync(0).toString().trim()
+
+for (let i=0; i<a.length; i++) {
+    let o = modify(a[i], a[-i])
+}
+
+if (o = true) console.log("Yes")
+else console.log("No")
+
+function modify(a,b) {
+    let o = false
+    if (a==b) true
+
+    return o
+}
