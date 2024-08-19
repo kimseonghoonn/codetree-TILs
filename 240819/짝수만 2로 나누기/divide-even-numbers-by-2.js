3502e23d13ec53cd05c89af52f93191c78f45af3
kimseonghoonn <kdr05103@naver.com>
2024-08-19 15:20:41 +0900

    Title: 짝수만 2로 나누기, Time: 174ms, Memory: 14MB, Status: Passed - CodeTree

--- 240819/짝수만 2로 나누기/divide-even-numbers-by-2.js
+++ 240819/짝수만 2로 나누기/divide-even-numbers-by-2.js
@@ -7,13 +7,11 @@ let str=" "
 let mod = 0
 for (let i=0; i<n; i++) {
     mod = modify(a[i])
-    str += mod + " "
 }
 
 console.log(str)
 
 function modify(a) {
     if (a%2==0 && a!=1) a=a/2
-
-    return a
+    process.stdout.write(a + " "); 
 }
